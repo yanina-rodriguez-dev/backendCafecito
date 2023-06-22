9edb86224e49fd770657d85fd154c6db6d94248f
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-22 18:29:27 -0300

    agregamos un puerto a la configuracion del index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 //usar un puerto 
 const app =express();
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), (=>{
+    console.log('Estoy en el puerto'+app.get('port'))
+}))
+
 //middlewares: funciones que se ejecutan antes de las rutas 
 
 //rutas
