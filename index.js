e0e26e698c76f36b5acd1fa62bbe5f762c9f8ddc
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-06-27 17:07:26 -0300

    se terminaron las configuraciones iniciales

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import cors from 'cors'; 
+import morgan from 'morgan';
+import path from 'path';
+
 //usar un puerto 
 const app =express();
+
+
 app.set('port', process.env.PORT || 4000);
-app.listen(app.get('port'), (=>{
+app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto'+app.get('port'))
-}))
+})
 
 //middlewares: funciones pequeñas que se ejecutan antes de las rutas  son para tareas especificas
 app.use(cors());//permitior conexiones remotas en mi backend (usa cors) 
+app.use(express.json())////permite  a mi app recibir objetos de tipo json en los request 
+app.use(morgan('dev')) //me muestra en la consola informacion extra de las solicitudes get-post - put etc
+console.log(__dirname); // la variable __dirname siempre devuelve l ruta absoluta de donde vive el 
+app.use(express.static( path.join(__dirname, '/public')))// esto nos permite poder ejecutar los archivos de mi proyecto en la ruta raiz
+ej: http
+
 
 //rutas
