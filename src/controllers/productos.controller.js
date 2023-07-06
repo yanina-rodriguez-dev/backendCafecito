051fc02126f3959295eba66d38485dc3ec990c05
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-07-06 08:40:03 -0300

    agrego validaciones

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -1,21 +1,29 @@
+import { validationResult } from "express-validator";
 import Producto from "../models/producto";
 
-export const obtenerProductos = (req, res)=>{
-    res.send('Se realizo la peticion get');
-}
+export const obtenerProductos = (req, res) => {
+  res.send("Se realizo la peticion get");
+};
 
-export const crearProducto = async (req, res)=>{
-    try{
-// console.log(req.body);
-const productoNuevo = new Producto(req.body)
-await productoNuevo.save()
-res.status(200).json({
-    mensaje: 'El producto se creo correctamente'
-})
-    }catch(error){
-        console.log(error)
-    res.status(404).json({
-        mensaje: 'Error alcrear el producto'
-    })
+export const crearProducto = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (errors.isEmpty === false) {
+        return res.status(400).json({
+            errores:errors.array()
+        })
+    } else {
     }
-}
+    // console.log(req.body);-
+    const productoNuevo = new Producto(req.body) | m0;
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: "El producto se creo correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error alcrear el producto",
+    });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,13 +1,56 @@
 import { Router } from "express";
+import { obtenerProductos } from "../controllers/productos.controller";
+import { check } from "express-validator";
 
 
-const router = Router()
+const router = Router();
 
 // app.get('/productos', (req, res)=>{
 //     res.send('Se hizo la peticion GET')
 // })
-router.route('/productos').get(app.get('/productos', (req, res)=>{
-    res.send('Se hizo la peticion GET')
-}))
+router
+  .route("/productos")
+  .get(obtenerProductos)
+  .post(
+    [
+      check("nombreProducto")
+        .notEmpty()
+        .withMessage("El nombre del producto es obligatorio")
+        .isLength({ min: 2, max: 100 })
+        .withMessage(
+          "El nombre del producto debe tener entre 2 a 100 caracteres"
+        ),
+      check("precio")
+        .notEmpty()
+        .withMessage("El precio es un dato obligatorio")
+        .isNumeric()
+        .withMessage("El precio debe ser un numero")
+        .custom((value) => {
+          if (value >= 1 && value <= 10000) {
+            return true;
+          } else {
+            throw new Error("El precio debe estar entre 1 y 10000");
+          }
+        }),
+        check('imagen')
+        .notEmpty()
+        .withMessage('La url de la imagen es una es un dato obligatorio')
+        .matches (/"%(?<=src=\")([^\"])+(png|jpg|gif)%i"/)
+        .withMessage("DEBE SER UNA URL DE IMAGEN VALIDA"),
+check('categoria')
+    .notEmpty()
+    .withMessage('La cat es obligatoria')
+    .isIn(['bebida caliente', 'bebida fria', 'dulce', 'salado'])
+     .withMessage('Debe ingresar una categoria valida')
+    ], 
 
-export default Router
+    crearProducto
+  );
+
+router
+  .route("/productos/:id")
+  .get(obtenerProductos)
+  .delete(borrarProducto)
+  .put(editarProducto);
+
+export default router;
